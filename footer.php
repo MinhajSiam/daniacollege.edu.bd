37e8fc54faf3d3342db6fcc1622a8c196bf4701e
MinhajSiam <minhajsiam007@gmail.com>
2025-09-27 02:16:09 +0600

    feat: add header and footer structure to the website

---
+++ footer.php
@@ -0,0 +1,14 @@
+</main>
+<footer>
+    <div class="footer-container">
+        <p>&copy; 2025 My Website. All rights reserved.</p>
+        <div class="social-links">
+            <a href="#">Facebook</a>
+            <a href="#">Instagram</a>
+            <a href="#">Twitter</a>
+        </div>
+    </div>
+</footer>
+</body>
+
+</html>
